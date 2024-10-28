e3ae395731e90ece2f690802e407c81ebd7d6bfa
Your Name <Juan.prida@adp.com  git config --global user.name jprida  git config --global user.email Juan.prida@adp.com>
2024-10-28 14:51:53 +0100

    Hardcode cloudinary thumbnail size to 400x400

--- proyecto_mas_hogar/settings.py
+++ proyecto_mas_hogar/settings.py
@@ -144,7 +144,6 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': "dkr8kjavi",
     'API_KEY': "438767119697134",
@@ -152,8 +151,9 @@ CLOUDINARY_STORAGE = {
 }
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+MEDIA_URL = 'https://res.cloudinary.com/eff5a268b180af5a3d418548329229/media/'
 
-MEDIA_URL = '/media/'
+# MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
