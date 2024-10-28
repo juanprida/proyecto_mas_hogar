f271921fe969346f945d57db6308a971fcb27210
Your Name <Juan.prida@adp.com  git config --global user.name jprida  git config --global user.email Juan.prida@adp.com>
2024-10-28 16:10:22 +0100

    Update filters #1000

--- app/templatetags/cloudinary_filters.py
+++ app/templatetags/cloudinary_filters.py
@@ -4,14 +4,15 @@ from cloudinary.utils import cloudinary_url
 register = template.Library()
 
 @register.filter(name='cloudinary_thumbnail')
-def cloudinary_thumbnail(image_url):
+def cloudinary_thumbnail(cloudinary_field):
     options = {
-        'crop': 'fill',
-        'gravity': 'auto',
+        'crop': 'fill',          # Crop the image to fill the dimensions
+        'gravity': 'auto',       # Focus on the important part of the image
         'width': 500,
         'height': 500,
         'format': 'jpg',
         'quality': 'auto'
     }
-    thumbnail_url, options = cloudinary_url(image_url, **options)
+    public_id = cloudinary_field.public_id
+    thumbnail_url, _ = cloudinary_url(public_id, **options)
     return thumbnail_url
